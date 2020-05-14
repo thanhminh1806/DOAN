f5521796762edfcf73376ba13d14c123f9db3329
Pham Thanh Minh (LTV-HN) <minh.pham@fssc.com.vn>
2020-05-14 17:52:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ApiService.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.HTTP;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface ApiService {
+
+}
---
+++ app/src/main/java/com/example/myapplication/GlobalObject.java
@@ -0,0 +1,5 @@
+package com.example.myapplication;
+
+class GlobalObject {
+    public static final String BASE_URL = "???????";
+}
