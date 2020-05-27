1ba356ebeddf0b0a5d3cb4748911db213d863a72
- <->
2020-05-27 13:54:59 +0700

    fix some iss

--- app/src/main/java/com/example/myapplication/GlobalObject.java
+++ app/src/main/java/com/example/myapplication/GlobalObject.java
@@ -3,6 +3,6 @@ package com.example.myapplication;
 import java.util.UUID;
 
 public class GlobalObject {
-    public static final String BASE_URL = "http://192.168.40.108:9000/";
+    public static final String BASE_URL = "http://192.168.5.4:9000/";
     public static String REGISTER_ID = "vanxjnkdep199x";
 }
